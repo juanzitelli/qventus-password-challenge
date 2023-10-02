66a83a39a9183e3b341a8362c00242e6448ef778
Juan Agustin Zitelli <juanzitelli7@gmail.com>
2023-10-02 16:22:16 -0300

    add manual testing capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Password } from "./components/password";
 
+const BASE_REQUIREMENTS = {
+  hasOneOrMoreSpecialCharacters: {
+    message: "Has at least one special character like $ or @.",
+  },
+  hasADigit: {
+    message: "Has at least a digit like 1, 2 or 3.",
+  },
+  hasUppercaseLetter: {
+    message: "Has at least one uppercase letter like A, B or C.",
+  },
+  hasNoConsecutiveLetters: {
+    message: "Doesn't have two consecutive letters like aa or BB.",
+  },
+};
+
 export const App = () => {
+  const [validateOnBlur, setValidateOnBlur] = useState(false);
+  const [selectedRequirements, setSelectedRequirements] = useState<
+    Record<keyof typeof BASE_REQUIREMENTS, boolean>
+  >(() => ({
+    hasOneOrMoreSpecialCharacters: true,
+    hasADigit: true,
+    hasUppercaseLetter: true,
+    hasNoConsecutiveLetters: true,
+  }));
+
+  const reqsChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSelectedRequirements((ps) => ({
+      ...ps,
+      [e.target.name]: e.target.checked,
+    }));
+  };
+
+  const filteredRequirements = Object.keys(selectedRequirements)
+    .filter(
+      (key) => selectedRequirements[key as keyof typeof selectedRequirements]
+    )
+    .reduce((obj, key) => {
+      obj[key as keyof typeof BASE_REQUIREMENTS] =
+        BASE_REQUIREMENTS[key as keyof typeof BASE_REQUIREMENTS];
+      return obj;
+    }, {} as typeof BASE_REQUIREMENTS);
+
   return (
     <main className="flex flex-col p-3 gap-10 h-screen w-screen justify-center items-center">
       <h1 className="text-4xl font-bold">Password Component</h1>
@@ -17,24 +59,77 @@ export const App = () => {
         }}
       >
         <Password
-          requirements={{
-            hasOneOrMoreSpecialCharacters: {
-              message: "Has at least one special character like $ or @.",
-            },
-            hasADigit: {
-              message: "Has at least a digit like 1, 2 or 3.",
-            },
-            hasUppercaseLetter: {
-              message: "Has at least one uppercase letter like A, B or C.",
-            },
-            hasNoConsecutiveLetters: {
-              message: "Doesn't have two consecutive letters like aa or BB.",
-            },
-          }}
+          requirements={filteredRequirements}
           id="password"
           name="password"
-          validateOnBlur={true}
+          validateOnBlur={validateOnBlur}
         />
+      </form>
+
+      <form className="flex flex-col">
+        <h2 className="text-2xl">Functionality toggle</h2>
+        <hr className="border-2 border-black" />
+        <label
+          className="flex flex-row gap-3 py-1"
+          htmlFor="hasOneOrMoreSpecialCharacters"
+        >
+          <input
+            type="checkbox"
+            onChange={reqsChangeHandler}
+            name="hasOneOrMoreSpecialCharacters"
+            id="hasOneOrMoreSpecialCharacters"
+            defaultChecked={
+              selectedRequirements["hasOneOrMoreSpecialCharacters"]
+            }
+          />
+          hasOneOrMoreSpecialCharacters
+        </label>
+        <label className="flex flex-row gap-3 py-1" htmlFor="hasADigit">
+          <input
+            type="checkbox"
+            onChange={reqsChangeHandler}
+            name="hasADigit"
+            id="hasADigit"
+            defaultChecked={selectedRequirements["hasADigit"]}
+          />
+          hasADigit
+        </label>
+        <label
+          className="flex flex-row gap-3 py-1"
+          htmlFor="hasUppercaseLetter"
+        >
+          <input
+            type="checkbox"
+            onChange={reqsChangeHandler}
+            name="hasUppercaseLetter"
+            id="hasUppercaseLetter"
+            defaultChecked={selectedRequirements["hasUppercaseLetter"]}
+          />
+          hasUppercaseLetter
+        </label>
+        <label
+          className="flex flex-row gap-3 py-1"
+          htmlFor="hasNoConsecutiveLetters"
+        >
+          <input
+            type="checkbox"
+            onChange={reqsChangeHandler}
+            name="hasNoConsecutiveLetters"
+            id="hasNoConsecutiveLetters"
+            defaultChecked={selectedRequirements["hasNoConsecutiveLetters"]}
+          />
+          hasNoConsecutiveLetters
+        </label>
+        <label className="flex flex-row gap-3 py-1" htmlFor="validateOnBlur">
+          <input
+            type="checkbox"
+            onChange={() => setValidateOnBlur(!validateOnBlur)}
+            name="validateOnBlur"
+            id="validateOnBlur"
+            defaultChecked={false}
+          />
+          validateOnBlur
+        </label>
       </form>
     </main>
   );
