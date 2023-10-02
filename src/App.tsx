546cb541e5b53228d26980841eb079526dbfbe53
Juan Agustin Zitelli <juanzitelli7@gmail.com>
2023-10-02 00:50:32 -0300

    import and use PassowrdInput

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,26 @@ import { PasswordInput } from "./PasswordInput";
 export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Password component</h1>
+      <PasswordInput
+        requirements={{
+          hasOneOrMoreSpecialCharacters: {
+            message:
+              "Password should include at least one special character like $ or @",
+          },
+          hasADigit: {
+            message: "Password should include at least a digit like 1, 2 or 3",
+          },
+          hasUppercaseLetter: {
+            message:
+              "Password should include at least one uppercase letter like A, B or C",
+          },
+          hasNoConsecutiveLetters: {
+            message:
+              "Password shouldn't include two consecutive letters like aa or BB",
+          },
+        }}
+      />
     </>
   );
 };
