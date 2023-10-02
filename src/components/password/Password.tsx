fb6c5af4fdcdaba8997ba4980e35a4781b30c51d
Juan Agustin Zitelli <juanzitelli7@gmail.com>
2023-10-02 14:08:31 -0300

    accept classes customization

--- src/components/password/Password.tsx
+++ src/components/password/Password.tsx
@@ -6,17 +6,52 @@ export const Password = ({
   validateOnBlur,
   id,
   name,
+  customClassNames: {
+    input: {
+      inputClassName,
+      containerClassName,
+      labelClassName,
+      toggleShowClassName,
+    },
+    errors: { listElementClassName, listItemClassName },
+  } = {
+    input: {},
+    errors: {},
+  },
 }: {
   requirements: Requirements;
   validateOnBlur: boolean;
   id: string;
   name: string;
+  customClassNames?: {
+    input: {
+      inputClassName?: string;
+      containerClassName?: string;
+      labelClassName?: string;
+      toggleShowClassName?: string;
+    };
+    errors: {
+      listElementClassName?: string;
+      listItemClassName?: string;
+    };
+  };
 }) => {
   return (
     <>
       <PasswordContextProvider requirements={requirements}>
-        <PasswordInput validateOnBlur={validateOnBlur} id={id} name={name} />
-        <PasswordErrors />
+        <PasswordInput
+          validateOnBlur={validateOnBlur}
+          id={id}
+          name={name}
+          containerClassName={containerClassName}
+          labelClassName={labelClassName}
+          toggleShowClassName={toggleShowClassName}
+          inputClassName={inputClassName}
+        />
+        <PasswordErrors
+          listElementClassName={listElementClassName}
+          listItemClassName={listItemClassName}
+        />
       </PasswordContextProvider>
     </>
   );
